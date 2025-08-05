74659c7481b070e6838acf372a85723fcefc5aca
Bazovichok <drinkopavelof@gmail.com>
2025-08-05 14:57:25 +0300

    визуальное обновление

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('dark-mode-toggle');
+    const isDark = localStorage.getItem('dark-mode') === 'true';
+
+    // Создать объекты Audio для звуков
+    const darkModeSound = new Audio('switch_dark_on.mp3');
+    const lightModeSound = new Audio('switch_light_on.mp3');
+
+
+    // Установить начальный режим
+    document.body.classList.toggle('dark-mode', isDark);
+    toggleButton.innerHTML = isDark ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+
+    // Обработчик нажатия на кнопку
+    toggleButton.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        const isDarkNow = document.body.classList.contains('dark-mode');
+        localStorage.setItem('dark-mode', isDarkNow);
+        this.innerHTML = isDarkNow ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+     
+        // Воспроизвести соответствующий звук
+        if (isDarkNow) {
+            darkModeSound.play();
+        } else {
+            lightModeSound.play();
+        }
+
+   });
+});
